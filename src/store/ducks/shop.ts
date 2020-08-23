6dadd65f6b94372279d58f41a0aa593e8608e25f
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-08-23 21:31:08 +0600

    Add initial shop reducer

---
+++ src/store/ducks/shop.ts
@@ -0,0 +1,79 @@
+import { AnyAction, Store } from 'redux';
+import SeamlessImmutable from 'seamless-immutable';
+
+/** interface for ShopItem  object */
+interface ShopItem {
+  _id: string;
+  title: string;
+  description: string;
+  picture: string;
+  price: number;
+  stock: number;
+}
+
+/** The reducer name */
+export const reducerName = 'shop';
+
+// actions
+/** action types */
+export const SET_SHOP_ITEMS = 'shop/reducer/shop/SET_SHOP_ITEMS';
+
+/** interface for SET_SHOP_ITEMS action */
+export interface SetShopItemsAction extends AnyAction {
+  items: ShopItem[];
+  type: typeof SET_SHOP_ITEMS;
+}
+
+/** Create type for reducer actions */
+export type ShopActionTypes = SetShopItemsAction | AnyAction;
+
+// action creators
+
+/** set shop items action creator
+ * @param {ShopItem[]} items - shop items to be set
+ * @returns {SetShopItemsAction} - an action to set items in store
+ */
+export const setShopItems = (items: ShopItem[]): SetShopItemsAction => ({
+  items,
+  type: SET_SHOP_ITEMS,
+});
+
+// the reducer
+
+/** interface for shop state in redux store */
+interface ShopState {
+  shopItems: ShopItem[];
+}
+
+/** Create an immutable shop state */
+export type ImmutableShopState = SeamlessImmutable.ImmutableObject<ShopState>;
+
+/** initial shop state */
+const initialState: ImmutableShopState = SeamlessImmutable({
+  shopItems: [],
+});
+
+/** the shop reducer function */
+export default function reducer(
+  state: ImmutableShopState = initialState,
+  action: ShopActionTypes
+): ImmutableShopState {
+  switch (action.type) {
+    case SET_SHOP_ITEMS:
+      return SeamlessImmutable({
+        shopItems: action.items,
+      });
+    default:
+      return state;
+  }
+}
+
+// selectors
+
+/** returns all the shop items
+ * @param {Partial<Store>} state - the redux store
+ * @returns { ShopItem[] } - the existing items
+ */
+export function getAllShopItems(state: Partial<Store>): ShopItem[] {
+  return (state as any)[reducerName].shopItems;
+}
