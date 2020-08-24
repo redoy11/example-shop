d200bfa5e2453cfea1e41a627597e4bae9f920e7
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-08-24 10:27:29 +0600

    Add initial CartItem

---
+++ src/Components/CartItem/CartItem.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ShopItem } from '../../store/ducks/shop';
+import {
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+  Icon,
+} from '@material-ui/core';
+
+/** Interface to describe CartItem props */
+interface CartItemProps extends ShopItem {
+  count: number;
+  addToCartHandler: any;
+}
+
+const CartItem: React.FC<CartItemProps> = (props: CartItemProps) => {
+  const { _id, title, price, count, addToCartHandler, picture } = props;
+  const removeHandler = () => {
+    addToCartHandler(_id, 0);
+  };
+  return (
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar variant="square" alt="" src={picture} />
+      </ListItemAvatar>
+      <ListItemText primary={title} secondary={`${count} x Tk ${price}`} />
+      <ListItemSecondaryAction>
+        <IconButton edge="start" aria-label="delete" onClick={removeHandler}>
+          <Icon>clear</Icon>
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+};
+
+export default CartItem;
