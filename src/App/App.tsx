8eade0d5870a2dd83c20ea6c651f9a0503551f31
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-08-24 11:50:18 +0600

    Add routing to product url

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Shelve from '../Containers/Shelve/Shelve';
+import ConnectedProduct from '../Containers/Product/Product';
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
       <Router>
         <Switch>
+          <Route path="/product/:id">
+            <ConnectedProduct />
+          </Route>
           <Route path="/">
             <Shelve />
           </Route>
